f7f3ddd6cf86ad76429d28ccecc9b74028374e17
Alhosainy Altaher <alhosainy.altaher@gmail.com>
2022-06-19 18:45:07 -0400

    Completed color change app functionality through useState & useEffect hooks in functional component App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [color, setColor] = useState("black"); 
+
+  useEffect(() => {
+    const changeColorOnClick = () => {
+      if (color === "black")
+        setColor("darkred");
+      else if (color === "darkred")
+        setColor("darkgreen");
+      else if (color === "darkgreen")
+        setColor("darkorange");
+      else if (color === "darkorange")
+        setColor("darkblue");
+      else if (color === "darkblue")
+        setColor("purple");
+      else 
+        setColor("black");
+    };
+  
+    document.addEventListener("click", changeColorOnClick);
+
+    return () => { //useEffect hook return statement runs when unmounted
+      document.removeEventListener("click", changeColorOnClick);
+    };
+  }, [color]); //useEffect hook runs when color changes
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="myDiv"
+        style={{
+          color: "white",
+          width: "200px",
+          height: "200px",
+          position: "absolute",
+          left: "44%",
+          top: "33%",
+          backgroundColor: color,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>Click on me!
+      </div>
     </div>
   );
-}
+};
 
 export default App;
